4a7335d6d444149447a2f10df09af3a578ed81aa
Dhruv Dabhi <dhruvdabhi101@gmail.com>
2024-08-29 22:44:13 +0530

    done

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Dhruv Dabhi';
+export const SITE_DESCRIPTION = 'Dhruv is a software engineer who loves to build things.';
